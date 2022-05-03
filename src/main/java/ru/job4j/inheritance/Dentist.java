c1c8ad254923b56961c695dd3c1850644f0db0a1
niklnn <nicklnn@ya.ru>
2022-05-03 14:52:03 +0300

    3. Реализация профессий в коде [#6837]

---
+++ src/main/java/ru/job4j/inheritance/Dentist.java
@@ -0,0 +1,7 @@
+package ru.job4j.inheritance;
+
+public class Dentist extends Doctor {
+    public Dentist(String name, String surname, String education, int birthday) {
+        super(name, surname, education, birthday);
+    }
+}
---
+++ src/main/java/ru/job4j/inheritance/Programmer.java
@@ -0,0 +1,7 @@
+package ru.job4j.inheritance;
+
+public class Programmer extends Engineer {
+    public Programmer(String name, String surname, String education, int birthday, int experience) {
+        super(name, surname, education, birthday, experience);
+    }
+}
